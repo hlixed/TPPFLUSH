a69cf7f001a3cccbe020dc7c83a73871a0d7e38c
tzst <hlixed@protonmail.com>
2017-06-12 21:26:34 -0400

    Add rudimentary interactive controller

---
+++ interactive_buttons.py
@@ -0,0 +1,29 @@
+import sys, time
+from lumainput import *
+
+if len(sys.argv) < 2:
+	print("To run as an executable: python3 lumainput.py <3ds ip>")
+	quit()
+
+server = sys.argv[1]
+
+server = LumaInputServer(server)
+server.hid_press(HIDButtons.X)
+server.send()
+
+def quick_press(button):
+	global server
+	server.hid_press(button)
+	server.send()
+	time.sleep(0.3)
+	server.hid_unpress(button)
+	server.send()
+
+while True:
+	btn = input(">")
+	if btn.lower() == 'b':
+		quick_press(HIDButtons.A)
+	if btn.lower() == 'b':
+		quick_press(HIDButtons.X)
+	if btn.lower() == 'b':
+		quick_press(HIDButtons.B)
